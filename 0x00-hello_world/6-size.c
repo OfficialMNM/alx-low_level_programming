0221be31422b43eb21d1c7d2a8e846076cbb767a
OfficialMNM <methusellanyongesa004@gmail.com>
2023-10-05 08:12:07 -0700

    Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/**
+  * main - prints the sizes of various types
+  * Return: 0 exit code for the program
+  */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	return (0);
+}
