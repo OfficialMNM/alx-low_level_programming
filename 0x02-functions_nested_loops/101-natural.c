5d874763e67eebafd5aa64be00114604b0d27e66
OfficialMNM <methusellanyongesa004@gmail.com>
2023-10-11 03:31:22 -0700

    Functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
-/* main- checks for multiples of 3 and 5
- * returns 0 on success
+/**
+ * main- checks for multiples of 3 and 5
+ * below 1024 and finds their sum
+ * returns: 0 on successful execution
  */
 
 int main(void)
 {
-    int b, c;
-    for (b = 1; b < 1024; b++)
-    {
-        if ((b % 3 == 0) || (b % 5 == 0))
-        {
-            c += b;
-        }
-        
-    }
-    printf("%d\n", c);
-    return (0);
-    
+	int b, c;
+
+	for (b = 0; b < 1024; b++)
+	{
+		if ((b % 3 == 0) || (b % 5 == 0))
+		{
+			c += b;
+		}
+	}
+
+	printf("%d\n", c);
+	return (0);
 }
